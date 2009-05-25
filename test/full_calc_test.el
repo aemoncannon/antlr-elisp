f6d81ce5521eb6e5018946a9f897eaa4d8b144a8
Aemon Cannon <aemoncannon@gmail.com>
2009-05-25 11:53:08 -0400

    Add a test of no-viable-alt at a token. Add template code for signalling no-viable-alt.

--- test/full_calc_test.el
+++ test/full_calc_test.el
@@ -21,6 +21,10 @@
       (assert-equal "More functions"
                     nil
                     (do-parse 'full_calc_elispLexer 'full_calc_elispParser 'evaluate "(ln 10 + (PI + 2)) * 3.2"))
+
+      (assert-error "No viable alternative at token"
+                    'no-viable-alt
+                    (do-parse 'full_calc_elispLexer 'full_calc_elispParser 'evaluate "(ln 10 + (PI + 2)) * ln"))
       )
 
 
