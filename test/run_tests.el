57a545ba35ad12c8f97ba8d959416f1f1a1e51a1
Aemon Cannon <aemoncannon@gmail.com>
2009-06-01 22:24:56 -0400

    Fix max-specpdl-size error by increasing value. Increase max-lisp-eval-depth while we're at it.'

--- test/run_tests.el
+++ test/run_tests.el
@@ -27,6 +27,8 @@
 ;;; Code:
 
 (setq debug-on-error t)
+(setq max-specpdl-size 5000)
+(setq max-lisp-eval-depth 1000)
 (setq load-path (cons (expand-file-name "build/test/grammars") load-path))
 (setq load-path (cons (expand-file-name "src/runtime/ELisp") load-path))
 (setq load-path (cons (expand-file-name "test") load-path))
